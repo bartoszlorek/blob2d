d886631303e6942baf5a7f0c45952307191d856f
Bartosz Lorek <bartosz.lorek@brainly.com>
2021-01-12 14:04:06 +0100

    Add Tilemap Searching Methods (#3)
    * add closest
    
    * add raytrace
    
    * stop ray trace at the obstacle
    
    * update comment

--- src/Tilemap.spec.ts
+++ src/Tilemap.spec.ts
@@ -32,4 +32,187 @@ describe('Tilemap()', () => {
 
     it('re-calculates bounds', () => {});
   });
+
+  describe('closest()', () => {
+    // prettier-ignore
+    const values3 = [
+      1, 2, 3,
+      4, 5, 6,
+      7, 8, 9
+    ];
+
+    // prettier-ignore
+    test.each([
+      [ // middle
+        [1, 1], [1, 2, 3, 
+                 4, 5, 6,
+                 7, 8, 9]
+      ],
+      [ // top-left
+        [0, 0], [0, 0, 0,
+                 0, 1, 2,
+                 0, 4, 5]
+      ],
+      [ // top-left (shifted)
+        [-1, -1], [0, 0, 0,
+                   0, 0, 0,
+                   0, 0, 1]
+      ],
+      [ // top-right
+        [2, 0], [0, 0, 0,
+                 2, 3, 0,
+                 5, 6, 0]
+      ],
+      [ // top-right (shifted)
+        [3, -1], [0, 0, 0,
+                  0, 0, 0,
+                  3, 0, 0]
+      ],
+      [ // bottom-right
+        [2, 2], [5, 6, 0,
+                 8, 9, 0,
+                 0, 0, 0]
+      ],
+      [ // bottom-right (shifted)
+        [3, 3], [9, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+      ],
+      [ // bottom-left
+        [0, 2], [0, 4, 5,
+                 0, 7, 8,
+                 0, 0, 0]
+      ],
+      [ // bottom-left (shifted)
+        [-1, 3], [0, 0, 7,
+                  0, 0, 0,
+                  0, 0, 0]
+      ],
+      [ // far shifted
+        [10, 3], [0, 0, 0,
+                  0, 0, 0,
+                  0, 0, 0]
+      ],
+    ])('returns closest values from 3×3 grid for %p', ([x, y], result) => {
+      const map = new Tilemap(values3, 3);
+      expect(map.closest(x, y)).toEqual(result);
+    });
+
+    // prettier-ignore
+    const values4 = [
+      1, 2, 3, 4,
+      5, 6, 7, 8,
+      9, 1, 2, 3,
+      4, 5, 6, 7
+    ];
+
+    // prettier-ignore
+    test.each([
+      [ // top-left
+        [1, 1], [1, 2, 3,
+                 5, 6, 7,
+                 9, 1, 2]
+        ],
+      [ // bottom-right
+        [2, 2], [6, 7, 8,
+                 1, 2, 3,
+                 5, 6, 7]
+      ],
+    ])('returns closest values from 4×4 grid for %p', ([x, y], result) => {
+      const map = new Tilemap(values4, 4);
+      expect(map.closest(x, y)).toEqual(result);
+    });
+
+    it('returns closest values for vertical grid', () => {
+      // prettier-ignore
+      const map = new Tilemap([
+        1,
+        2,
+        3,
+        4,
+        5
+      ], 1);
+
+      // prettier-ignore
+      expect(map.closest(0, 0)).toEqual([
+        0, 0, 0,
+        0, 1, 0,
+        0, 2, 0
+      ]);
+    });
+
+    it('returns closest values for horizontal grid', () => {
+      const map = new Tilemap([1, 2, 3, 4, 5], 5);
+
+      // prettier-ignore
+      expect(map.closest(0, 0)).toEqual([
+        0, 0, 0,
+        0, 1, 2,
+        0, 0, 0
+      ]);
+    });
+  });
+
+  describe('raytrace()', () => {
+    // prettier-ignore
+    const emptyValues = [
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
+    ];
+
+    test.each`
+      a         | b         | length
+      ${[0, 0]} | ${[3, 0]} | ${3}
+      ${[0, 0]} | ${[2, 1]} | ${2}
+      ${[0, 0]} | ${[3, 3]} | ${3}
+      ${[0, 0]} | ${[1, 2]} | ${2}
+      ${[0, 0]} | ${[0, 3]} | ${3}
+      ${[3, 3]} | ${[0, 0]} | ${3}
+      ${[3, 3]} | ${[1, 1]} | ${2}
+    `(
+      'returns $length as traversed length between $a and $b in empty space',
+      ({a: [ax, ay], b: [bx, by], length}) => {
+        const map = new Tilemap(emptyValues, 4);
+        expect(map.raytrace(ax, ay, bx, by)).toBe(length);
+      }
+    );
+
+    // prettier-ignore
+    const filledValues = [
+      1, 1, 1, 0,
+      0, 0, 1, 0,
+      0, 0, 1, 0,
+      0, 1, 1, 1
+    ];
+
+    test.each`
+      a         | b         | length
+      ${[0, 1]} | ${[3, 1]} | ${-2}
+      ${[0, 3]} | ${[3, 0]} | ${-2}
+      ${[1, 2]} | ${[3, 0]} | ${-1}
+    `(
+      'returns $length as negative length to obstacle between $a and $b',
+      ({a: [ax, ay], b: [bx, by], length}) => {
+        const map = new Tilemap(filledValues, 4);
+        expect(map.raytrace(ax, ay, bx, by)).toBe(length);
+      }
+    );
+
+    it('returns 0 when starting tile is the same as target', () => {
+      const map = new Tilemap([0, 0], 2);
+      expect(map.raytrace(0, 0, 0, 0)).toBe(0);
+    });
+
+    it('returns 0 when starting tile is fulfilled', () => {
+      const map = new Tilemap([1, 1], 2);
+      expect(map.raytrace(0, 0, 1, 0)).toBe(0);
+    });
+
+    it('returns positive length when target tile is fulfilled', () => {
+      const map = new Tilemap([0, 0, 1], 3);
+      expect(map.raytrace(0, 0, 2, 0)).toBe(2);
+    });
+  });
 });
--- src/Tilemap.ts
+++ src/Tilemap.ts
@@ -28,6 +28,87 @@ export class Tilemap<
     this.calculateBoundingBox();
   }
 
+  public removeByIndex(index: number): void {
+    this.values[index] = 0;
+    this.calculateBoundingBox();
+  }
+
+  public getIndex(x: number, y: number): number {
+    return x + this.dimension * y;
+  }
+
+  public getPoint(index: number): VectorType {
+    this._point[0] = index % this.dimension;
+    this._point[1] = Math.floor(index / this.dimension);
+    return this._point;
+  }
+
+  public closest(x: number, y: number): number[] {
+    const arr = this._closestArray;
+
+    const start0 = this.getIndex(x - 1, y - 1);
+    const start1 = this.getIndex(x - 1, y);
+    const start2 = this.getIndex(x - 1, y + 1);
+
+    const row0 = y - 1 >= 0;
+    const row1 = y >= 0;
+    const row2 = y + 1 >= 0;
+
+    const col0 = !(x - 1 < 0 || x - 1 >= this.dimension);
+    const col1 = !(x < 0 || x >= this.dimension);
+    const col2 = !(x + 1 < 0 || x + 1 >= this.dimension);
+
+    arr[0] = row0 && col0 ? this.values[start0] || 0 : 0;
+    arr[1] = row0 && col1 ? this.values[start0 + 1] || 0 : 0;
+    arr[2] = row0 && col2 ? this.values[start0 + 2] || 0 : 0;
+
+    arr[3] = row1 && col0 ? this.values[start1] || 0 : 0;
+    arr[4] = row1 && col1 ? this.values[start1 + 1] || 0 : 0;
+    arr[5] = row1 && col2 ? this.values[start1 + 2] || 0 : 0;
+
+    arr[6] = row2 && col0 ? this.values[start2] || 0 : 0;
+    arr[7] = row2 && col1 ? this.values[start2 + 1] || 0 : 0;
+    arr[8] = row2 && col2 ? this.values[start2 + 2] || 0 : 0;
+
+    return arr;
+  }
+
+  // based on Bresenham’s Line Generation Algorithm
+  public raytrace(x0: number, y0: number, x1: number, y1: number): number {
+    const deltaX = Math.abs(x1 - x0);
+    const deltaY = Math.abs(y1 - y0);
+    const directionX = x0 < x1 ? 1 : -1;
+    const directionY = y0 < y1 ? 1 : -1;
+
+    let error = deltaX - deltaY;
+    let length = 0;
+    let x = x0;
+    let y = y0;
+
+    while (true) {
+      if (x === x1 && y === y1) {
+        return length;
+      }
+
+      if (this.values[this.getIndex(x, y)] > 0) {
+        return -length || 0;
+      }
+
+      const error2 = 2 * error;
+      length += 1;
+
+      if (error2 > -deltaY) {
+        error -= deltaY;
+        x += directionX;
+      }
+
+      if (error2 < deltaX) {
+        error += deltaX;
+        y += directionY;
+      }
+    }
+  }
+
   protected calculateBoundingBox(): void {
     if (this.values.length === 0) {
       this.width = 0;
