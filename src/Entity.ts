7b3cdb2404b370b16355ac7d10a672a939de14c2
Bartosz Lorek <bartosz.lorek@brainly.com>
2021-01-14 09:00:21 +0100

    change order of actions in entity update

--- src/Entity.ts
+++ src/Entity.ts
@@ -30,18 +30,18 @@ export class Entity<
   }
 
   public update(deltaTime: number): void {
-    // set position based on velocity from the previous frame
-    this.translateX(this.velocity[0] * deltaTime);
-    this.translateY(this.velocity[1] * deltaTime);
-
     // invoke traits that may change velocity for the next
     // frame or correct display position for the current one
     for (let i = 0; i < this._traits.length; i++) {
       this._traits[i].update(deltaTime);
     }
 
-    // change visuals of display object based on the final position
+    // render based on the current frame position
     this.updateDisplayPosition();
+
+    // set position for next frame based on velocity
+    this.translateX(this.velocity[0] * deltaTime);
+    this.translateY(this.velocity[1] * deltaTime);
   }
 
   public destroy() {
