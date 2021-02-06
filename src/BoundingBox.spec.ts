b8c26f27eb371e73d7dbe62207455311d0e41928
Bartosz Lorek <bartosz.lorek@brainly.com>
2021-02-06 22:47:02 +0100

    update tests

--- src/BoundingBox.spec.ts
+++ src/BoundingBox.spec.ts
@@ -21,4 +21,16 @@ describe('BoundingBox()', () => {
     clone.copy(bbox);
     expect(bbox).toEqual(clone);
   });
+
+  it('updates max values when width or height has changed', () => {
+    const bbox = new BoundingBox(
+      [49.9483005251519, 65.72052564862729],
+      [81.9483005251519, 97.72052564862729]
+    );
+
+    bbox.width = 8;
+    bbox.height = 8;
+    expect(bbox.max[0]).toBe(57.9483005251519);
+    expect(bbox.max[1]).toBe(73.72052564862729);
+  });
 });
