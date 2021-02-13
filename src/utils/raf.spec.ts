e099017df4c63cc4f08b206dca62ab2bba9bae45
Bartosz Lorek <bartosz.lorek@brainly.com>
2021-02-13 14:28:36 +0100

    add raf utils tests

---
+++ src/utils/raf.spec.ts
@@ -0,0 +1,93 @@
+import {setFrameTimeout, setFrameInterval, clearFrameRequest} from './raf';
+
+let timestamp = 0;
+const now = jest.spyOn(window.performance, 'now');
+const raf = jest.spyOn(window, 'requestAnimationFrame');
+
+now.mockReturnValue(timestamp);
+raf.mockImplementation(() => 12340); // request id
+
+const advanceAnimationFrames = value => {
+  const calls = raf.mock.calls;
+  const listener = calls[calls.length - 1][0];
+  timestamp += value;
+  listener(timestamp);
+};
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  timestamp = 0;
+});
+
+describe('setFrameTimeout()', () => {
+  it('fires callback after a delay', () => {
+    const callback = jest.fn();
+
+    setFrameTimeout(callback, 300);
+    advanceAnimationFrames(100);
+    advanceAnimationFrames(100);
+    advanceAnimationFrames(100);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not fire callback before a delay', () => {
+    const callback = jest.fn();
+
+    setFrameTimeout(callback, 300);
+    advanceAnimationFrames(100);
+    advanceAnimationFrames(100);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe('setFrameInterval()', () => {
+  it('fires callback after each cycle', () => {
+    const callback = jest.fn();
+
+    setFrameInterval(callback, 200);
+    advanceAnimationFrames(100); // first cycle
+    advanceAnimationFrames(100);
+    advanceAnimationFrames(100); // second cycle
+    advanceAnimationFrames(100);
+
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+
+  it('does not fire callback before a delay', () => {
+    const callback = jest.fn();
+
+    setFrameTimeout(callback, 300);
+    advanceAnimationFrames(100);
+    advanceAnimationFrames(100);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe('clearFrameRequest()', () => {
+  it('does not throw an error when request is undefined', () => {
+    expect(() => {
+      clearFrameRequest(undefined);
+    }).not.toThrowError();
+  });
+
+  it('prevents setFrameTimeout to execute', () => {
+    const callback = jest.fn();
+    const request = setFrameTimeout(callback, 300);
+
+    clearFrameRequest(request);
+    advanceAnimationFrames(500);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+
+  it('prevents setFrameInterval to execute', () => {
+    const callback = jest.fn();
+    const request = setFrameInterval(callback, 300);
+
+    clearFrameRequest(request);
+    advanceAnimationFrames(500);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+});
--- src/utils/raf.ts
+++ src/utils/raf.ts
@@ -4,55 +4,44 @@ export interface IAnimationFrameRequest {
 
 export function setFrameTimeout(
   callback: () => void,
-  delay: number
+  delay: number = 0
 ): IAnimationFrameRequest {
-  const initial = performance.now();
   const request = {canceled: false};
+  const initial = performance.now();
 
-  function listener() {
-    if (request.canceled === false) {
-      const current = performance.now();
-
-      if (current - initial >= delay) {
-        callback();
-      } else {
-        window.requestAnimationFrame(listener);
-      }
+  function listener(timestamp: number) {
+    if (request.canceled === true) return;
+    if (timestamp - initial >= delay) {
+      callback();
+    } else {
+      window.requestAnimationFrame(listener);
     }
   }
 
-  listener();
+  listener(initial);
   return request;
 }
 
 export function setFrameInterval(
   callback: () => void,
-  delay: number
+  delay: number = 0
 ): IAnimationFrameRequest {
-  let initial = performance.now();
   const request = {canceled: false};
+  let initial: number = performance.now();
 
-  function listener() {
-    if (request.canceled === false) {
-      const current = performance.now();
-
-      if (current - initial >= delay) {
-        initial = current;
-        callback();
-      }
-
-      window.requestAnimationFrame(listener);
+  function listener(timestamp: number) {
+    if (request.canceled === true) return;
+    if (timestamp - initial >= delay) {
+      initial = timestamp;
+      callback();
     }
+    window.requestAnimationFrame(listener);
   }
 
-  listener();
+  listener(initial);
   return request;
 }
 
-export function clearFrameRequest(
-  request: IAnimationFrameRequest | null | void
-) {
-  if (request) {
-    request.canceled = true;
-  }
+export function clearFrameRequest(request: IAnimationFrameRequest | void) {
+  if (request) request.canceled = true;
 }
